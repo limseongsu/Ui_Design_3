4baf3f1419fb9de0e41d8c08a1f09d89cd733360
imseongsu <tjdtn2323@naver.com>
2021-06-07 09:18:17 +0900

    first

---
+++ lib/main/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Main extends StatelessWidget {
+  const Main({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
